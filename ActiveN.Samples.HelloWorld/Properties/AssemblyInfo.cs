bff726143f4e70345f7b56a9ca05db2fa5c70905
Simon Mourier <smourier@users.noreply.github.com>
2025-08-26 13:29:02 +0200

    wip

--- ActiveN.Samples.HelloWorld/Properties/AssemblyInfo.cs
+++ ActiveN.Samples.HelloWorld/Properties/AssemblyInfo.cs
@@ -3,11 +3,6 @@
 #else
 [assembly: AssemblyConfiguration("RELEASE")]
 #endif
-[assembly: AssemblyTitle("ActiveN.Samples.HelloWorld")]
-[assembly: AssemblyDescription("A sample for ActiveN framework (for building ActiveX controls in .NET Core AOT).")]
-[assembly: AssemblyCompany("Simon Mourier")]
-[assembly: AssemblyProduct("ActiveN")]
-[assembly: AssemblyCopyright("Copyright (C) 2024-2025 Simon Mourier. All rights reserved.")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("88a202b6-a9d7-4aaa-b06a-db28e256b151")]
