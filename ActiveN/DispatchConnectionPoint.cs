c08b63acf8ed72f52ff0bbd8dc1a77361c9fd997
Simon Mourier <simon_mourier@hotmail.com>
2026-09-20 16:18:41 +0200

    Update nugets

--- ActiveN/DispatchConnectionPoint.cs
+++ ActiveN/DispatchConnectionPoint.cs
@@ -15,29 +15,34 @@ public partial class DispatchConnectionPoint(Guid sourceInterfaceId) : BaseConne
         TracingUtilities.Trace($"dispid {dispId} with {parameters?.Length ?? 0} parameters. Sinks: {Sinks.Count}");
         if (Sinks.Count > 0)
         {
+            var count = parameters?.Length ?? 0;
             Variant[]? variants = null;
-            VARIANT[]? vars;
-            var dp = new DISPPARAMS();
-            if (parameters?.Length > 0)
+            var vars = new VARIANT[count];
+            if (count > 0)
             {
-                variants = new Variant[parameters.Length];
-                vars = new VARIANT[parameters.Length];
-                for (var i = 0; i < parameters.Length; i++)
+                variants = new Variant[count];
+                for (var i = 0; i < count; i++)
                 {
-                    variants[i] = new Variant(parameters[i]);
+                    variants[i] = new Variant(parameters![i]);
                     vars[i] = variants[i].Detached;
                 }
-
-                dp.cArgs = (uint)parameters.Length;
-                dp.rgvarg = vars.AsPointer();
             }
 
             try
             {
-                foreach (var kv in Sinks)
+                fixed (VARIANT* pVars = vars)
                 {
-                    var disp = kv.Value.As<IDispatch>();
-                    disp?.Object.Invoke(dispId, Guid.Empty, 0, DISPATCH_FLAGS.DISPATCH_METHOD, dp, 0, 0, 0);
+                    var dp = new DISPPARAMS
+                    {
+                        cArgs = (uint)count,
+                        rgvarg = (nint)pVars,
+                    };
+
+                    foreach (var kv in Sinks)
+                    {
+                        var disp = kv.Value.As<IDispatch>();
+                        disp?.Object.Invoke(dispId, Guid.Empty, 0, DISPATCH_FLAGS.DISPATCH_METHOD, dp, 0, 0, 0);
+                    }
                 }
             }
             finally
